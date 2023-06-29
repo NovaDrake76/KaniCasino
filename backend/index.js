5c183c473841e517a72db86849d7170a92d7cd7c
Nathan <novadrake78@gmail.com>
2023-06-29 03:19:09 -0300

    fix: removed port

--- backend/index.js
+++ backend/index.js
@@ -52,7 +52,7 @@ crash(io);
 
 const port = process.env.PORT || 5000;
 
-server.listen(port, '0.0.0.0', () => {
+server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
