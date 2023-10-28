f9dd58620c05fc4244b6e19ab2ac4f46afb4771b
Nathan Marques <nathan.marques176@gmail.com>
2023-10-28 10:31:23 -0300

    fix: unused useeffects

--- src/pages/Upgrade/Items.tsx
+++ src/pages/Upgrade/Items.tsx
@@ -1,4 +1,4 @@
-import { useState, useContext, useEffect } from "react";
+import { useContext, useEffect } from "react";
 import UserContext from "../../UserContext";
 import UserItems from "./UserItems";
 import ChooseUpgradeItems from "./ChooseUpgradeItems";
--- src/pages/Upgrade/Upgrade.tsx
+++ src/pages/Upgrade/Upgrade.tsx
@@ -1,5 +1,5 @@
 
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Title from "../../components/Title";
 import { Tooltip } from "react-tooltip";
 import Items from "./Items";
