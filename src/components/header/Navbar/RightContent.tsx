0f0f3a50d10617ba5404ca585286ec455d4af259
Nathan Marques <nathan.marques176@gmail.com>
2023-12-18 00:02:04 -0300

    fix: unused var

--- src/components/header/Navbar/RightContent.tsx
+++ src/components/header/Navbar/RightContent.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React from "react"
 import Avatar from "../../Avatar";
 import { FaRegBell } from "react-icons/fa";
 import { FaRegBellSlash } from "react-icons/fa";
