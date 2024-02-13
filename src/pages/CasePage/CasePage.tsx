028c427e8315aff06f53fd3117f89fcc407f0b7b
Nathan Marques <nathan.marques176@gmail.com>
2024-02-13 00:25:09 -0300

    fix: quantity at case page

--- src/pages/CasePage/CasePage.tsx
+++ src/pages/CasePage/CasePage.tsx
@@ -21,7 +21,7 @@ const CasePage = () => {
   const [animationAux, setAnimationAux] = useState<boolean>(false);
   const [animationAux2, setAnimationAux2] = useState<boolean>(false);
   const [loadingButton, setLoadingButton] = useState<boolean>(false);
-  const [quantity, _setQuantity] = useState<number>(1);
+  const [quantity, setQuantity] = useState<number>(1);
 
   const { userData, toogleUserFlow } = useContext(UserContext);
 
