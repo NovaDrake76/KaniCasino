c58eadba673d6db1a31d79740ec2298303dbf7d3
Nathan Marques <nathan.marques176@gmail.com>
2025-06-20 08:51:46 -0300

    back online

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,18 +1,18 @@
 import { useState, useEffect } from "react";
-// import Banner from "./Banner";
-// import CaseListing from "./CaseListing";
-// import GameListing from "./GamesListing";
-// import Leaderboard from "./Leaderboard";
+import Banner from "./Banner";
+import CaseListing from "./CaseListing";
+import GameListing from "./GamesListing";
+import Leaderboard from "./Leaderboard";
 import { getCases } from "../../services/cases/CaseServices";
-// import Skeleton from "react-loading-skeleton";
+import Skeleton from "react-loading-skeleton";
 import { toast } from "react-toastify";
-// import { BannerProps } from "./Types";
-// import { Carousel } from "react-responsive-carousel";
+import { BannerProps } from "./Types";
+import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 
 const Home = () => {
-  const [_cases, setCases] = useState<any>();
-  const [_loading, setLoading] = useState<boolean>(true);
+  const [cases, setCases] = useState<any>();
+  const [loading, setLoading] = useState<boolean>(true);
 
   const getNewCases = async () => {
     setLoading(true);
@@ -30,75 +30,49 @@ const Home = () => {
     getNewCases();
   }, []);
 
-  // const BannerContent: BannerProps[] = [
-  //   {
-  //     left: {
-  //       image: "/images/marisaBanner.webp",
-  //       title: "CRASH GAME",
-  //       description: "Don't burn, fly high! Try your luck now!",
-  //       link: "/crash",
-  //     },
-  //     right: (
-  //       <div>
-  //         <img src="/images/crashBannerTitle.webp" alt="upgrade" />
-  //       </div>
-  //     ),
-  //   },
-  //   {
-  //     left: {
-  //       image: "/images/paris.webp",
-  //       title: "NEW UPGRADE GAME",
-  //       description: "Go big or go home. Try your luck now!",
-  //       link: "/upgrade",
-  //     },
-  //     right: null,
-  //   },
-  //   {
-  //     left: {
-  //       image: "/images/homeBanner.webp",
-  //       //if title is hide, it will hide the information component on the left side
-  //       title: "hide",
-  //       description: "Try your luck now!",
-  //       link: "/slots",
-  //     },
-  //     right: (
-  //       <div className="hidden 2xl:flex 2xl:mr-36">
-  //         <img src="/images/KANICASINO.webp" alt="kanicasino" />
-  //       </div>
-  //     ),
-  //   },
-  // ];
+  const BannerContent: BannerProps[] = [
+    {
+      left: {
+        image: "/images/marisaBanner.webp",
+        title: "CRASH GAME",
+        description: "Don't burn, fly high! Try your luck now!",
+        link: "/crash",
+      },
+      right: (
+        <div>
+          <img src="/images/crashBannerTitle.webp" alt="upgrade" />
+        </div>
+      ),
+    },
+    {
+      left: {
+        image: "/images/paris.webp",
+        title: "NEW UPGRADE GAME",
+        description: "Go big or go home. Try your luck now!",
+        link: "/upgrade",
+      },
+      right: null,
+    },
+    {
+      left: {
+        image: "/images/homeBanner.webp",
+        //if title is hide, it will hide the information component on the left side
+        title: "hide",
+        description: "Try your luck now!",
+        link: "/slots",
+      },
+      right: (
+        <div className="hidden 2xl:flex 2xl:mr-36">
+          <img src="/images/KANICASINO.webp" alt="kanicasino" />
+        </div>
+      ),
+    },
+  ];
 
   return (
     <div className="w-screen flex justify-center">
       <div className=" flex-col w-full max-w-[1920px] ">
-        <div className="flex flex-col md:flex-row  justify-center">
-          <div className="flex ">
-            <div className="flex flex-col justify-center items-center gap-4">
-              <h1 className="text-5xl font-bold text-white">Hello!</h1>
-              <p className="text-white max-w-xs text-center">
-                <b>Kanicasino</b> is not available anyomore. It's too expensive
-                to run, I'm sorry.
-              </p>
-              <p className="text-white">
-                <b>Thank you for playing.</b> I hope you enjoyed it.
-              </p>
-              <div className="flex w-full justify-end">
-                <p className="text-xs text-gray-500">
-                  Kanicasino - Made with love by novadrake76
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="flex flex-col justify-center">
-            <img src="/images/boo.webp" alt="kogasa fumo" />
-            <p className="text-xs text-gray-500 text-center ml-40">
-              source{" "}
-              <a href="https://www.pixiv.net/en/artworks/119007909">here</a>
-            </p>
-          </div>
-        </div>
-        {/* <Carousel
+        <Carousel
           autoPlay={true}
           infiniteLoop={true}
           showThumbs={false}
@@ -107,15 +81,10 @@ const Home = () => {
           showArrows={false}
           interval={7000}
           stopOnHover={false}
-
         >
-          {
-            BannerContent.map((_item, index) => (
-              <Banner key={index} left={_item.left} right={_item.right} />
-            ))
-          }
-
-
+          {BannerContent.map((_item, index) => (
+            <Banner key={index} left={_item.left} right={_item.right} />
+          ))}
         </Carousel>
         {loading ? (
           <div className="flex items-center justify-center w-full mt-[164px]">
@@ -137,13 +106,12 @@ const Home = () => {
           </div>
         ) : (
           <CaseListing
-          name="NEW CASES"
-          cases={cases.length > 6 ? cases.slice(0, 6) : cases}
+            name="NEW CASES"
+            cases={cases.length > 6 ? cases.slice(0, 6) : cases}
           />
         )}
         <GameListing name="Our Games" />
         <Leaderboard />
- */}
       </div>
     </div>
   );
